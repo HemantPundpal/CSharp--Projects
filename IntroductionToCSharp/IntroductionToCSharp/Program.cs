0484ee1ecfd123e08490250eb200eca48313ac4d
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-01-30 12:54:07 +1300

    Introduction To CSharp

---
+++ IntroductionToCSharp/IntroductionToCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C#, What is your first name?");
+        string UserFirstName = Console.ReadLine();
+        Console.WriteLine("Welcome to C#, What is your Last name?");
+        string UserLastName = Console.ReadLine();
+        Console.WriteLine("Hello " + UserFirstName + " " + UserLastName);
+        Console.WriteLine("Hello {0} {1}", UserFirstName, UserLastName);
+        Console.WriteLine($"Hello {UserFirstName} {UserLastName}");
+        Console.ReadKey();
+    }
+}
+

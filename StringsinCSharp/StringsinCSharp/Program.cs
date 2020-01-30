e2bc42828226867d43449d1b9041639107bac87b
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-01-30 13:20:59 +1300

    Introduction To CSharp

---
+++ StringsinCSharp/StringsinCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace StringsinCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userString1 = "C#"; //this is immutable string, thus every change to this string will have a new string object in memory. The older objects will stay in memory untill garbage collector cleans them.
+            userString1 += "video";
+            userString1 += "Tutorial";
+            userString1 += "for";
+            userString1 += "Begineers";
+
+            StringBuilder userString2 = new StringBuilder("C#"); // this is mutable string, thus every change to this string will be made to the same StringBuilder object.
+            userString2.Append("video");
+            userString2.Append("Tutorial");
+            userString2.Append("for");
+            userString2.Append("begineers");
+
+
+            // Thus if you have extensive manupulations of string in your application. Then using StringBuilder is more effecient for heap as well as cpu.
+        }
+    }
+}
